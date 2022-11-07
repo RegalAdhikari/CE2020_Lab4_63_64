e7247a45b78479ec1c99f3b45d6a967eecfbfc43
Manish Shivabhakti <manish@learnr.app>
2022-11-07 15:07:53 +0545

    Fix typo

--- include/AbstractBST.h
+++ include/AbstractBST.h
@@ -1,22 +1,18 @@
-
-
 #ifndef AbstractBST.h
-#define AbstractBSt.h
+#define AbstractBSt .h
 
 class AbstractBST
 {
 private:
     /* data */
 public:
-    virtual bool isEmpty()=0;
-    virtual void add(int Key, int Value)=0;
-    virtual void max(int &output)=0;
-    virtual void min(int &output)=0;
-    virtual bool exists(int targetKey)=0;
+    virtual bool isEmpty() = 0;
+    virtual void add(int Key, int Value) = 0;
+    virtual void max(int &output) = 0;
+    virtual void min(int &output) = 0;
+    virtual bool exists(int targetKey) = 0;
 
-    virtual void inorder()=0;
+    virtual void inorder() = 0;
 };
-
-
 
 #endif
