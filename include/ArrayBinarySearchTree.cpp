e1cea0153284a5398f3ffc8108e4737125279314
RegalAdhikari <regalbrt@gmail.com>
2022-11-19 22:34:19 +0545

    Added Search and Delete

--- include/ArrayBinarySearchTree.cpp
+++ include/ArrayBinarySearchTree.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int tree [10];
 int leftNode = 0;
 int rightNode = 0;
+int set_right(int key, int parent);
 int root(int key)
 {
     if (tree[0] != '\0')
@@ -28,7 +29,7 @@ void isEmpty()
 int new_Node(int key, int parent){
     if (key >=parent)
     {
-        /* code */  //I give up :(
+        /* code */
     }
     return 0;
 }
@@ -36,8 +37,10 @@ int new_Node(int key, int parent){
 int set_left(int key, int parent) {
   if (tree[parent] == '\0')
     cout << "\nCan't set child at "<< (parent * 2) + 1<< " , no parent found";
+    
   else
     tree[(parent * 2) + 1] = key;
+
   return 0;
 }
  
@@ -59,12 +62,47 @@ int print_tree() {
   }
   return 0;
 }
+void removeBST(int key)
+{
+  int replacementKey = key;
+  for (int i = 0; i < 10; i++)
+  {
+    if (tree[i] == key)
+    {
 
+      tree[i] = NULL;
+      break;
+    }
+    else if(i==9)
+    {
+      cout<<"The given key doesn't exist in Tree";
+    }
+  }  
+}
+void searchBST()
+{
+  int searchKey;
+  cout<<"Enter a key to search: ";
+  cin>>searchKey;
+  for (int i = 0; i < 10; i++)
+  {
+    if (tree[i] == searchKey)
+    {
+      cout<<"The key exists at the node :"<< i;
+      break;
+    }
+    else if(i==9) {
+      cout<<"The key doesn't exists in the tree";
+      break;
+    }
+  }  
+}
 // Driver Code
 int main() 
 {
     int count;
     int arr[10];
+    int deleteKey;
     isEmpty();
     cout<<"\nHow many nodes of the tree?";
     cin>>count;    
@@ -73,42 +111,50 @@ int main()
         cout<<"Enter Element "<<r<<":";
         cin>> arr[r];
     }
-    //sorting kaam nai lagena
-    // for (int i = 0; i < count; i++)
-    // {
-    //     int k = arr[i];
-    //     int j = i;
-    //     while (j>0 && arr[j-1]>k)
-    //     {
-    //         arr[j] = arr[j-1];
-    //         j=j-1;
-    //     }
-    //     arr[j] = k;
-        
-    // }
-    // cout<<"\nSorted array:";
-    // for (int r = 0; r < count; r++)
-    // {
-    //     cout<<arr[r]<<",";
-    // }
+
     root(arr[0]);
     int pp= 0;
-    for (int i = 0; i < count; i++)
+    for (int i = 1; i < count; i++)
     {
-        if (arr[i]<arr[pp])
+      // if (i>2)
+      //   {
+      //     pp++;
+      //   }
+      if (arr[i]<arr[pp])
         {
-            set_left(arr[i],pp);
+          if (tree[1]!='\0' && arr[i]<tree[1])
+          {
+            set_left(arr[i],1);  
+          }
+          else if (tree[1]!='\0' && arr[i]>tree[1])
+          {
+            set_right(arr[i],1);  
+          }
+          else
+            set_left(arr[i],0);
         }
-        else
+      else
         {
-            set_right(arr[i], pp);
-        }
-        
+          if (tree[2]!='\0' && arr[i]>tree[2])
+          {
+            set_right(arr[i],2);            
+          }
+          else if (tree[2]!='\0' && arr[i]<tree[2])
+          {
+            set_left(arr[i],2);  
+          }
+          else
+          set_right(arr[i],0);
+        }   
     }
-    
     
 
     // set_left(1,0);
+  print_tree();
+  searchBST();
+  cout<<"\nKey to delete: ";
+  cin>>deleteKey;
+  removeBST(deleteKey);
   print_tree();
   return 0;
 }
